2811f19e9f22f5c515301af0cca9d7c63759d764
Timothy D Meadows II <timothy@timothymeadows.com>
2017-10-22 12:08:25 -0700

    Add concurrency to managed memory.

--- src/Data/ManagedMemory.cs
+++ src/Data/ManagedMemory.cs
@@ -49,8 +49,42 @@ namespace Blackfeather.Data
     /// </summary>
     public sealed class ManagedMemory : IDisposable
     {
-        private bool _disposed = false;
-        private List<ManagedMemorySpace> _memory = new List<ManagedMemorySpace>();
+        private bool _disposed;
+        private ConcurrentDictionary<Guid, ManagedMemorySpace> _memory = new ConcurrentDictionary<Guid, ManagedMemorySpace>();
+
+        /// <summary>
+        /// Copy an entry directly out of memory. Optionally you can supply ManagedMemorySpace to T for Reads.
+        /// </summary>
+        /// <param name="location">Guid location in memory.</param>
+        /// <returns>A memory entry.</returns>
+        public ManagedMemorySpace Copy(Guid location)
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException("Memory");
+            }
+
+            if (!_memory.Any())
+            {
+                return default(ManagedMemorySpace);
+            }
+
+            var memoryEntry = _memory.FirstOrDefault(entry => entry.Key.Equals(location));
+            if (memoryEntry.Value.Equals(default(ManagedMemorySpace)))
+            {
+                return memoryEntry.Value;
+            }
+
+            return new ManagedMemorySpace
+            {
+                Pointer = memoryEntry.Value.Pointer,
+                Name = memoryEntry.Value.Name,
+                Value = memoryEntry.Value.Value,
+                Created = memoryEntry.Value.Created,
+                Updated = memoryEntry.Value.Updated,
+                Accessed = memoryEntry.Value.Accessed
+            };
+        }
 
         /// <summary>
         /// Managed memory read.
@@ -61,32 +95,29 @@ namespace Blackfeather.Data
         /// <returns>A memory value with type T.</returns>
         public T Read<T>(string pointer, string name)
         {
-            lock (_memory)
+            if (_disposed)
             {
-                if (_disposed)
-                {
-                    throw new ObjectDisposedException("Memory");
-                }
-
-                if (!_memory.Any())
-                {
-                    return default(T);
-                }
-
-                var memorySet = _memory.Where(entry => entry.Pointer == pointer)
-                    .Where(entry => entry.Name == name);
-                if (!_memory.Any())
-                {
-                    return default(T);
-                }
-
-                var accessedEntry = memorySet.FirstOrDefault();
-                accessedEntry.Accessed = DateTime.UtcNow.ToBinary();
-
-                return typeof(T) == typeof(ManagedMemorySpace)
-                    ? (T)Convert.ChangeType(accessedEntry, typeof(ManagedMemorySpace))
-                    : (T)Convert.ChangeType(accessedEntry.Value, typeof(T));
+                throw new ObjectDisposedException("Memory");
             }
+
+            if (!_memory.Any())
+            {
+                return default(T);
+            }
+
+            var memoryEntry = _memory.FirstOrDefault(entry => entry.Value.Pointer == pointer && entry.Value.Name == name);
+            if (memoryEntry.Value.Equals(default(ManagedMemorySpace)))
+            {
+                return default(T);
+            }
+
+            var memory = Copy(memoryEntry.Key);
+            memory.Accessed = DateTime.UtcNow.ToBinary();
+            _memory.TryUpdate(memoryEntry.Key, memory, memoryEntry.Value);
+
+            return typeof(T) == typeof(ManagedMemorySpace)
+                ? (T) Convert.ChangeType(memory, typeof(ManagedMemorySpace))
+                : (T) Convert.ChangeType(memory.Value, typeof(T));
         }
 
         /// <summary>
@@ -102,33 +133,26 @@ namespace Blackfeather.Data
                 throw new ObjectDisposedException("Memory");
             }
 
-            lock (_memory)
+            if (!_memory.Any())
             {
-                if (!_memory.Any())
-                {
-                    return null;
-                }
-
-                var memorySet = _memory.Where(entry => entry.Pointer == pointer);
-                if (!_memory.Any())
-                {
-                    return null;
-                }
-
-                var memoryFragment = new List<T>();
-                memorySet.All(entry =>
-                {
-                    entry.Accessed = DateTime.UtcNow.ToBinary();
-
-                    memoryFragment.Add(typeof(T) == typeof(ManagedMemorySpace)
-                        ? (T) Convert.ChangeType(entry, typeof(ManagedMemorySpace))
-                        : (T) Convert.ChangeType(entry.Value, typeof(T)));
-
-                    return true;
-                });
-
-                return memoryFragment.ToArray();
+                return null;
             }
+
+            var memoryEntries = _memory.Where(entry => entry.Value.Pointer == pointer);
+            var memoryFragment = new ConcurrentBag<T>();
+
+            foreach (var memoryEntry in memoryEntries)
+            {
+                var memory = Copy(memoryEntry.Key);
+                memory.Accessed = DateTime.UtcNow.ToBinary();
+                _memory.TryUpdate(memoryEntry.Key, memory, memoryEntry.Value);
+
+                memoryFragment.Add(typeof(T) == typeof(ManagedMemorySpace)
+                    ? (T)Convert.ChangeType(memory, typeof(ManagedMemorySpace))
+                    : (T)Convert.ChangeType(memory.Value, typeof(T)));
+            }
+
+            return memoryFragment.ToArray();
         }
 
         /// <summary>
@@ -140,7 +164,7 @@ namespace Blackfeather.Data
         /// <param name="created">Binary time the object was created. (Optional)</param>
         /// <param name="updated">Binary time the object was updated. (Optional)</param>
         /// <param name="accessed">Binary time the object was last accessed. (Optional)</param>
-        public void Write(string pointer, string name, object value, long created = 0, long updated = 0,
+        public Guid Write(string pointer, string name, object value, long created = 0, long updated = 0,
             long accessed = 0)
         {
             if (_disposed)
@@ -152,40 +176,51 @@ namespace Blackfeather.Data
             var createStamp = created == 0 ? DateTime.UtcNow.ToBinary() : created;
             var updateStamp = updated == 0 ? createStamp : updated;
             var accessedStamp = accessed == 0 ? createStamp : accessed;
+            var location = Guid.NewGuid();
 
-            lock (_memory)
+            _memory.TryAdd(location, new ManagedMemorySpace()
             {
-                _memory.Add(new ManagedMemorySpace()
-                {
-                    Created = createStamp,
-                    Updated = updateStamp,
-                    Accessed = accessedStamp,
-                    Pointer = pointer,
-                    Name = name,
-                    Value = value
-                });
-            }
+                Created = createStamp,
+                Updated = updateStamp,
+                Accessed = accessedStamp,
+                Pointer = pointer,
+                Name = name,
+                Value = value
+            });
+
+            return location;
         }
 
-        public void WriteAll(ManagedMemorySpace[] spaces)
+        public List<Guid> WriteAll(ManagedMemorySpace[] spaces)
         {
+            var locations = new List<Guid>();
             spaces.ToList()
-                .ForEach(
-                    entry => Write(entry.Pointer, entry.Name, entry.Value, entry.Created, entry.Updated, entry.Accessed));
+                .ForEach(entry =>
+                {
+                    var location = Write(entry.Pointer, entry.Name, entry.Value, entry.Created, entry.Updated,
+                        entry.Accessed);
+                    locations.Add(location);
+                });
+
+            return locations;
         }
 
-        public void WriteAll(List<ManagedMemorySpace> spaces)
+        public List<Guid> WriteAll(List<ManagedMemorySpace> spaces)
         {
-            spaces.ForEach(
-                entry => Write(entry.Pointer, entry.Name, entry.Value, entry.Created, entry.Updated, entry.Accessed));
-        }
-
-        public void WriteAll(string pointer, Dictionary<string, object> spaces)
-        {
-            foreach (var space in spaces)
+            var locations = new List<Guid>();
+            spaces.ForEach(entry =>
             {
-                Write(pointer, space.Key, space.Value);
-            }
+                var location = Write(entry.Pointer, entry.Name, entry.Value, entry.Created, entry.Updated,
+                    entry.Accessed);
+                locations.Add(location);
+            });
+
+            return locations;
+        }
+
+        public List<Guid> WriteAll(string pointer, Dictionary<string, object> spaces)
+        {
+            return spaces.Select(space => Write(pointer, space.Key, space.Value)).ToList();
         }
 
         /// <summary>
@@ -193,28 +228,26 @@ namespace Blackfeather.Data
         /// </summary>
         /// <param name="pointer">A reference pointer to the memory entry.</param>
         /// <param name="name">A name pointer to the memory entry.</param>
-        public void Delete(string pointer, string name)
+        public Guid Delete(string pointer, string name)
         {
             if (_disposed)
             {
                 throw new ObjectDisposedException("Memory");
             }
 
-            lock (_memory)
+            if (!_memory.Any())
             {
-                if (!_memory.Any())
-                {
-                    return;
-                }
-
-                var memorySet = _memory.Where(entry => entry.Pointer == pointer).Where(entry => entry.Name == name);
-                if (!memorySet.Any())
-                {
-                    return;
-                }
-
-                _memory.Remove(memorySet.First());
+                return default(Guid);
             }
+
+            var memoryEntry = _memory.FirstOrDefault(entry => entry.Value.Pointer == pointer && entry.Value.Name == name);
+            if (memoryEntry.Value.Equals(default(ManagedMemorySpace)))
+            {
+                return default(Guid);
+            }
+
+            _memory.TryRemove(memoryEntry.Key, out var removed);
+            return memoryEntry.Key;
         }
 
 
@@ -222,64 +255,56 @@ namespace Blackfeather.Data
         /// Managed memory bulk delete.
         /// </summary>
         /// <param name="pointer">A reference pointer to the memory entry.</param>
-        public void DeleteAll(string pointer)
+        public List<Guid> DeleteAll(string pointer)
         {
-            lock (_memory)
+            if (_disposed)
             {
-                if (_disposed)
-                {
-                    throw new ObjectDisposedException("Memory");
-                }
-
-                if (!_memory.Any())
-                {
-                    return;
-                }
-
-                var memorySet = _memory.Where(entry => entry.Pointer == pointer);
-                memorySet.ToList().ForEach(entry => _memory.Remove(entry));
+                throw new ObjectDisposedException("Memory");
             }
+
+            if (!_memory.Any())
+            {
+                return default(List<Guid>);
+            }
+
+            var locations = new List<Guid>();
+            var memoryEntries = _memory.Where(entry => entry.Value.Pointer == pointer);
+            memoryEntries.ToList().ForEach(memoryEntry =>
+            {
+                _memory.TryRemove(memoryEntry.Key, out var removed);
+                locations.Add(memoryEntry.Key);
+            });
+
+            return locations;
         }
 
         public void Clear()
         {
-            lock (_memory)
-            {
-                _memory.Clear();
-            }
+            _memory.Clear();
         }
 
-        public ManagedMemorySpace[] Export()
+        public KeyValuePair<Guid, ManagedMemorySpace>[] Export()
         {
-            lock (_memory)
-            {
-                return _memory.ToArray();
-            }
+            return _memory.ToArray();
         }
 
-        public ManagedMemorySpace[] ExportAll(string pointer)
+        public KeyValuePair<Guid, ManagedMemorySpace>[] ExportAll(string pointer)
         {
-            lock (_memory)
-            {
-                return _memory.Where(entry => entry.Pointer == pointer).ToArray();
-            }
+            return _memory.Where(entry => entry.Value.Pointer == pointer).ToArray();
         }
 
-        public void Import(ManagedMemorySpace[] spaces, bool append = false)
+        public void Import(KeyValuePair<Guid, ManagedMemorySpace>[] spaces, bool append = false)
         {
-            lock (_memory)
+            if (append)
             {
-                if (append)
+                foreach (var space in spaces)
                 {
-                    foreach (var space in spaces)
-                    {
-                        _memory.Add(space);
-                    }
+                    _memory.TryAdd(space.Key, space.Value);
                 }
-                else
-                {
-                    _memory = new List<ManagedMemorySpace>(spaces);
-                }
+            }
+            else
+            {
+                _memory = new ConcurrentDictionary<Guid, ManagedMemorySpace>(spaces);
             }
         }
 
@@ -297,9 +322,11 @@ namespace Blackfeather.Data
             }
 
             var file = File.ReadAllBytes(path);
-            var encodedFile = string.Empty;
+            string encodedFile;
+
             switch (type)
             {
+                default:
                 case ContentDataType.Text:
                     encodedFile = new UTF8Encoding().GetString(file);
                     FromText(encodedFile, append);
@@ -350,12 +377,12 @@ namespace Blackfeather.Data
         /// <returns></returns>
         public string[] ToText(string pointer = null)
         {
-            var memoryPointer = new List<string> { "Pointer,Name,Value,Created,Updated,Accessed" };
+            var memoryPointer = new List<string> {"Pointer,Name,Value,Created,Updated,Accessed"};
             var memoryFragment = string.IsNullOrEmpty(pointer) ? Export() : ExportAll(pointer);
             memoryPointer.AddRange(
                 memoryFragment.Select(
                     entry =>
-                            $"\"{entry.Pointer}\",\"{entry.Name}\",\"{entry.Value}\",{entry.Created},{entry.Updated},{entry.Accessed}"));
+                        $"\"{entry.Value.Pointer}\",\"{entry.Value.Name}\",\"{entry.Value.Value}\",{entry.Value.Created},{entry.Value.Updated},{entry.Value.Accessed}"));
 
             return memoryPointer.ToArray();
         }
@@ -374,14 +401,13 @@ namespace Blackfeather.Data
 
             var payload = !value.Contains("\r\n")
                 ? value.Split('\n')
-                : value.Split(new[] { '\r', '\n' }, StringSplitOptions.None);
-            foreach (
-                var csv in
-                payload.Select(payloadLine => payloadLine.Split(','))
-                    .Where(csv => csv.Length == 6)
-                    .Where(csv => csv[5].ToLower() != "accessed"))
+                : value.Split(new[] {'\r', '\n'}, StringSplitOptions.None);
+
+            foreach (var csv in payload.Select(payloadLine => payloadLine.Split(',')).Where(csv => csv.Length == 6)
+                .Where(csv => csv[5].ToLower() != "accessed"))
             {
-                Write(csv[0], csv[1], csv[2], Convert.ToInt64(csv[3]), Convert.ToInt64(csv[4]), Convert.ToInt64(csv[5]));
+                Write(csv[0], csv[1], csv[2], Convert.ToInt64(csv[3]), Convert.ToInt64(csv[4]),
+                    Convert.ToInt64(csv[5]));
             }
         }
 
@@ -394,7 +420,7 @@ namespace Blackfeather.Data
         {
             var memorySpace = new MemoryStream();
             var memoryFilter = string.IsNullOrEmpty(pointer) ? Export() : ExportAll(pointer);
-            var contract = new DataContractJsonSerializer(typeof(ManagedMemorySpace[]), knownTypes);
+            var contract = new DataContractJsonSerializer(typeof(KeyValuePair<Guid, ManagedMemorySpace>[]), knownTypes);
             contract.WriteObject(memorySpace, memoryFilter.ToArray());
 
             var json = System.Text.Encoding.UTF8.GetString(memorySpace.ToArray());
@@ -411,8 +437,8 @@ namespace Blackfeather.Data
         public void FromJson(string value, bool append = false, params Type[] knownTypes)
         {
             var memorySpace = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(value));
-            var contract = new DataContractJsonSerializer(typeof(ManagedMemorySpace[]), knownTypes);
-            var memorySpaces = (ManagedMemorySpace[])contract.ReadObject(memorySpace);
+            var contract = new DataContractJsonSerializer(typeof(KeyValuePair<Guid, ManagedMemorySpace>[]), knownTypes);
+            var memorySpaces = (KeyValuePair<Guid, ManagedMemorySpace>[]) contract.ReadObject(memorySpace);
 
             memorySpace.Dispose();
             Import(memorySpaces, append);
@@ -428,13 +454,10 @@ namespace Blackfeather.Data
                 return;
             }
 
-            lock (_memory)
-            {
-                _memory.Clear();
-                _memory = null;
+            _memory.Clear();
+            _memory = null;
 
-                _disposed = true;
-            }
+            _disposed = true;
         }
     }
 }
